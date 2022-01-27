94ad2caf5d1666909b7fbb3a8de9746c0cc91908
timoMil <timo_milbich@gmx.de>
2022-01-27 16:54:41 +0100

    enable quantization of intermediate resnet layers (block_to_quantize); add monitoring of individual codeword usage; add partly batchnorm removal; update print summaries

--- architectures/resnet50.py
+++ architectures/resnet50.py
@@ -34,7 +34,7 @@ class Network(torch.nn.Module):
         if self.block_to_quantize < MAX_BLOCK_TO_QUANTIZE:
             assert '1x1conv' not in self.arch, "Expected dimesionality of intermediate features must be kept."
             self.e_dim = NUM_FEAT_PER_BLOCK[self.block_to_quantize]
-        else:
+        elif self.block_to_quantize > MAX_BLOCK_TO_QUANTIZE:
             raise Exception('Attempting to quantize non-existent resnet block [Max. number is 4.]!')
 
         # Add Vector Quantization (Optionally)
